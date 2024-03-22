57772cfdf61fcc86b0795ef1b0f735e242762dd0
SHIFA FATIMA <shifa.fatimeh@gmail.com>
2024-03-22 08:57:07 +0530

    bootstrap

---
+++ app.js
@@ -0,0 +1,7 @@
+let maox = document.querySelector(".ide");
+
+maox.addEventListener("click", () => {
+  let signinModal = new bootstrap.Modal(document.getElementById("SigninModal"));
+
+  signinModal.hide();
+});
